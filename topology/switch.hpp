d3e78d52872baa1c26821130f57ff15d83a84377
Raj Joshi <rajkiranjoshi@gmail.com>
2021-01-17 19:51:01 +0800

    Refactored routing: support intra-rack

--- topology/switch.hpp
+++ topology/switch.hpp
@@ -16,16 +16,15 @@ typedef std::unordered_map<host_id_t, host_tor_link_p> neighbor_host_table_t;
 typedef std::pair<host_id_t, host_tor_link_p> neighbor_host_table_pair;
 
 
-typedef struct routeScheduleInfo
-{
-    switch_p nextSwitch;
-    sim_time_t pktNextForwardTime;
-} routeScheduleInfo;
+struct routeScheduleInfo;
+typedef struct routeScheduleInfo routeScheduleInfo;
 
-
+struct Switch;
+typedef struct Switch Switch;
+typedef std::shared_ptr<Switch> switch_p;
 
 /* Switch struct */
-typedef struct Switch
+struct Switch
 {
     switch_id_t id;
     sim_time_t hop_delay;
@@ -38,19 +37,26 @@ typedef struct Switch
     void receiveTriggerPkt(triggerpkt_p pkt);
 
     /* Fills rinfo. Returns Success or Failure */
-    status_t routeNormalPkt(normalpkt_p pkt, routeScheduleInfo &rinfo);
-    status_t routeTriggerPkt(triggerpkt_p pkt, routeScheduleInfo &rinfo);
-    status_t routeToDstSwitch(switch_id_t dstSwitchId, routeScheduleInfo &rinfo);
+    status_t routeScheduleNormalPkt(normalpkt_p pkt, const sim_time_t pktArrivalTime, routeScheduleInfo &rsinfo);
+    status_t routeScheduleTriggerPkt(triggerpkt_p pkt, const sim_time_t pktArrivalTime, routeScheduleInfo &rsinfo);
+    
+    switch_p getNextHop(switch_id_t dstSwitchId);
+    void schedulePkt(const pkt_size_t pktsize, const sim_time_t pktArrivalTime, const link_speed_gbps_t linkSpeed, sim_time_t &qNextIdleTime);
+    /* Inter-switch routing+scheduling  */
+    status_t routeScheduleToDstSwitch(const pkt_size_t pktsize, const sim_time_t pktArrivalTime, const switch_id_t dstSwitchId, routeScheduleInfo &rsinfo, PacketType ptype); 
 
     Switch() = default; 
     Switch(switch_id_t id);
 
-} Switch;
-
-typedef std::shared_ptr<Switch> switch_p;
+};
 
 
-/* Routing Table struct */
+struct routeScheduleInfo
+{
+    switch_p nextSwitch;
+    sim_time_t pktNextForwardTime;
+};
+
 
 
 #endif
