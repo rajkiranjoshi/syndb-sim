d100d5a938bf6504865bf2c35aecdcb291fedf3d
Raj Joshi <rajkiranjoshi@gmail.com>
2021-02-03 13:18:00 +0800

    Toggle macro for file logging

--- simulation/config.hpp
+++ simulation/config.hpp
@@ -5,31 +5,36 @@
 #include "utils/types.hpp"
 // #include "topology/topology.hpp"
 
+#ifndef LOGGING
+#define LOGGING 0 
+#endif
+
 typedef struct Config 
 {
     static const sim_time_t timeIncrementNs = 100;
     const float totalTimeMSecs = 0.1;
 
     // IMPORTANT: update numHosts and numSwitches as per the topology
-    const TopologyType topoType = TopologyType::Simple;
-    // const TopologyType topoType = TopologyType::FatTree;
     
-    static const ft_scale_t fatTreeTopoK = 4; // Fat Tree scale k
-
-    // static const host_id_t numHosts = (fatTreeTopoK * fatTreeTopoK * fatTreeTopoK)/4;
-    // static const switch_id_t numSwitches = (fatTreeTopoK * fatTreeTopoK) + ((fatTreeTopoK * fatTreeTopoK)/4);
-    static const switch_id_t numSwitches = 3;
-    static const uint numHosts = 4;
+    /* SimpleTopo Params */
+    // const TopologyType topoType = TopologyType::Simple;
+    // static const switch_id_t numSwitches = 3;
+    // static const uint numHosts = 4;
+    // const TrafficPatternType trafficPatternType = TrafficPatternType::SimpleTopo;
+    
+    /* FatTree Topo Params */
+    const TopologyType topoType = TopologyType::FatTree;
+    static const ft_scale_t fatTreeTopoK = 24; // Fat Tree scale k
+    static const host_id_t numHosts = (fatTreeTopoK * fatTreeTopoK * fatTreeTopoK)/4;
+    static const switch_id_t numSwitches = (fatTreeTopoK * fatTreeTopoK) + ((fatTreeTopoK * fatTreeTopoK)/4);   
+    const TrafficPatternType trafficPatternType = TrafficPatternType::AlltoAll;
+    // const TrafficPatternType trafficPatternType = TrafficPatternType::FtUniform;
 
     // const TrafficGenType trafficGenType = TrafficGenType::Distribution;
     const TrafficGenType trafficGenType = TrafficGenType::Continuous;
     const pkt_size_t fixedPktSizeForSimpleTrafficGen = 1500;
 
-    const TrafficPatternType trafficPatternType = TrafficPatternType::SimpleTopo;
-    // const TrafficPatternType trafficPatternType = TrafficPatternType::AlltoAll;
-    // const TrafficPatternType trafficPatternType = TrafficPatternType::FtUniform;
-
-    static const uint numTriggersPerSwitchType = 15;
+    static const uint numTriggersPerSwitchType = 2;
 
     const link_speed_gbps_t torLinkSpeedGbps = 100;
     const link_speed_gbps_t networkLinkSpeedGbps = 100;
