4f1b3ba1e580d74b8234455c5972f7103698dbd7
Nishant Budhdev <nishant.budhdev@gmail.com>
2021-02-05 15:27:48 +0800

    Remove dumping of data to host files

--- utils/pktdumper.cpp
+++ utils/pktdumper.cpp
@@ -13,26 +13,25 @@ PktDumper::PktDumper(switch_id_t numberOfSwitches, host_id_t numberOfHosts) {
     this->prefixStringForFileName = "dump_" + std::to_string(calendarTime->tm_hour) + "_" + 
                                     std::to_string(calendarTime->tm_min) + "_" + 
                                     std::to_string(calendarTime->tm_sec) + "_";
-    #ifdef DEBUG
     ndebug_print("{}", this->prefixStringForFileName);
-    #endif
-
 
     // open all file pointers in write/output mode
     std::string triggerFileName = "./data/" + prefixStringForFileName + "trigger.txt";
     this->triggerFilePointer.open(triggerFileName, std::fstream::out);
 
+    ndebug_print("Number of Switches: {}", numberOfSwitches);
     for (int i = 0; i < numberOfSwitches; i++) {
         std::string fileName = prefixStringForFileName + "switch_" + std::to_string(i) + ".txt";
         std::fstream file ("./data/" + fileName, std::fstream::out);
         this->switchFilePointers.push_back(std::move(file));
     }
 
-    for (int i = 0; i < numberOfHosts; i++) {
-        std::string fileName = prefixStringForFileName + "host_" + std::to_string(i) + ".txt";
-        std::fstream file ("./data/" + fileName, std::fstream::out);
-        this->hostFilePointers.push_back(std::move(file));
-    }
+    ndebug_print("Number of Hosts: {}", numberOfHosts);
+    // for (int i = 0; i < numberOfHosts; i++) {
+    //     std::string fileName = prefixStringForFileName + "host_" + std::to_string(i) + ".txt";
+    //     std::fstream file ("./data/" + fileName, std::fstream::out);
+    //     this->hostFilePointers.push_back(std::move(file));
+    // }
 }
 
 
@@ -54,27 +53,19 @@ PktDumper::~PktDumper() {
 
 void PktDumper::dumpPacket(normalpkt_p pkt){
     
-    #if DEBUG
     ndebug_print_yellow("--------   HOSTS   ----------");
     ndebug_print("Start Time: {}", pkt->startTime);
-    #endif
-    this->hostFilePointers[pkt->srcHost] << pkt->startTime << "\t" << pkt->id << std::endl;
+    // this->hostFilePointers[pkt->srcHost] << pkt->startTime << "\t" << pkt->id << std::endl;
 
-    if (pkt->endTime != 0) {
-        #if DEBUG
-        ndebug_print("End Time: {}", pkt->endTime);
-        #endif
-        this->hostFilePointers[pkt->dstHost] << pkt->endTime << "\t" << pkt->id << std::endl;
-    }
+    // if (pkt->endTime != 0) {
+    //     ndebug_print("End Time: {}", pkt->endTime);
+    //     this->hostFilePointers[pkt->dstHost] << pkt->endTime << "\t" << pkt->id << std::endl;
+    // }
 
-    #if DEBUG
     ndebug_print_yellow("-------   SWITCHES   ---------");
-    #endif
     std::list<switchINTInfo>::iterator switchTimeStampsIterator;
     for (switchTimeStampsIterator = pkt->switchINTInfoList.begin(); switchTimeStampsIterator != pkt->switchINTInfoList.end(); switchTimeStampsIterator++) {
-        #if DEBUG
         ndebug_print("ID: {} \t Time: {}",switchTimeStampsIterator->swId, switchTimeStampsIterator->rxTime);
-        #endif
         this->switchFilePointers[switchTimeStampsIterator->swId] << switchTimeStampsIterator->rxTime << "\t" << pkt->id << "\t";
         this->switchFilePointers[switchTimeStampsIterator->swId] << pkt->srcHost << "\t" << pkt->dstHost << std::endl;
     }
