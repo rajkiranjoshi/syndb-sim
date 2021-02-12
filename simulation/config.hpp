c51d3af9d2e217fa532f87966feb30175db7a792
Raj Joshi <rajkiranjoshi@gmail.com>
2021-02-12 16:39:55 +0800

    config.hpp re-org. Set to 100ms FatTree expt

--- simulation/config.hpp
+++ simulation/config.hpp
@@ -3,9 +3,7 @@
 #endif
 
 #if PROFILING 
-
 #include "simulation/config_profiling.hpp"
-
 #else
 
 #ifndef CONFIG_H
@@ -24,7 +22,6 @@
 typedef struct Config 
 {
     static const sim_time_t timeIncrementNs = 100;
-    const float totalTimeMSecs = 100;
 
     // IMPORTANT: update numHosts and numSwitches as per the topology
     
@@ -33,43 +30,50 @@ typedef struct Config
     // static const switch_id_t numSwitches = 3;
     // static const uint numHosts = 4;
     // const TrafficPatternType trafficPatternType = TrafficPatternType::SimpleTopo;
+    // const sim_time_t triggerInitialDelay = 1500000; // 1.5ms for Line topo 10G runs
+    // const link_speed_gbps_t torLinkSpeedGbps = 100;
+    // const link_speed_gbps_t networkLinkSpeedGbps = 100;
+
+    /* LineTopo 10ms Expt Params */
+    /* Do NOT comment out */ const pkt_size_t fixedPktSizeForSimpleTrafficGen = 101; //1500;
+    // const float totalTimeMSecs = 10;
+    // const TopologyType topoType = TopologyType::Line;
+    // static const switch_id_t numSwitches = 5;
+    // static const uint numHosts = 2;
+    // const TrafficPatternType trafficPatternType = TrafficPatternType::SimpleTopo;
+    // const sim_time_t triggerInitialDelay = 1500000; // 1.5ms for Line topo 10ms run
+    // static const uint numTriggersPerSwitchType = 100;
+    // const link_speed_gbps_t torLinkSpeedGbps = 10;
+    // const link_speed_gbps_t networkLinkSpeedGbps = 10;
+    // const TrafficGenType trafficGenType = TrafficGenType::Continuous;
     
-    /* FatTree Topo Params */
+    
+    /* FatTree Topo 100ms Expt Params */
     /* Do NOT comment out */ static const ft_scale_t fatTreeTopoK = 24; // Fat Tree scale k
+    /* Do NOT comment out */ const uint8_t ftMixedPatternPercentIntraRack = 75;
+    /* Do NOT comment out */ const uint8_t targetBaseNetworkLoadPercent = 40;  /* 30 -> 25%, 40 -> 31%, 50 -> 36% */
+    const float totalTimeMSecs = 100;
     const TopologyType topoType = TopologyType::FatTree;
     static const host_id_t numHosts = (fatTreeTopoK * fatTreeTopoK * fatTreeTopoK)/4;
     static const switch_id_t numSwitches = (fatTreeTopoK * fatTreeTopoK) + ((fatTreeTopoK * fatTreeTopoK)/4);   
+    const TrafficPatternType trafficPatternType = TrafficPatternType::FtMixed;
+    const TrafficGenType trafficGenType = TrafficGenType::Distribution;
+    const sim_time_t triggerInitialDelay = 15000000; // 15ms for k=24 fatTree topo 100ms run
+    static const uint numTriggersPerSwitchType = 100;
+    const link_speed_gbps_t torLinkSpeedGbps = 100;
+    const link_speed_gbps_t networkLinkSpeedGbps = 100;
+    
+
     // const TrafficPatternType trafficPatternType = TrafficPatternType::AlltoAll;
     // const TrafficPatternType trafficPatternType = TrafficPatternType::FtUniform;
-    const TrafficPatternType trafficPatternType = TrafficPatternType::FtMixed;
 
-    const uint8_t ftMixedPatternPercentIntraRack = 75;
-
-    /* Trigger-related params */
-    static const uint numTriggersPerSwitchType = 100;
-    const sim_time_t triggerInitialDelay = 15000000; // 15ms for k=24 fatTree topo 100ms run
-
-    const TrafficGenType trafficGenType = TrafficGenType::Distribution;
-    /* 
-    targetBaseNetworkLoadPercent -> observed network load
-        30 -> 25% 
-        40 -> 31%
-        50 -> 36%
-    */
-    const uint8_t targetBaseNetworkLoadPercent = 40;
-    
-    // const TrafficGenType trafficGenType = TrafficGenType::Continuous;
-    /* Do NOT comment out */ const pkt_size_t fixedPktSizeForSimpleTrafficGen = 1500;
 
     /* Incast Related Params */
     const uint8_t percentIncastTime = 10;
     const host_id_t incastFanInRatio = numHosts / 4; // 25% of the total hosts
     const host_id_t percentTargetIncastHosts = 30;
 
-    const link_speed_gbps_t torLinkSpeedGbps = 100;
-    const link_speed_gbps_t networkLinkSpeedGbps = 100;
-
-    const load_t hostTrafficGenLoadPercent = 100;
+    // const load_t hostTrafficGenLoadPercent = 100;
 
     const sim_time_t switchHopDelayNs = 1000;
     const sim_time_t minSwitchHopDelayNs = 950;
